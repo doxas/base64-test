01b876d45fd3596525d2753d853cf3e0640eb1f8
doxas <eizanco@gmail.com>
2021-03-16 14:43:01 +0900

    implement todataurl method with path

--- b64.js
+++ b64.js
@@ -1,5 +1,13 @@
 
 export class B64 {
+    static toDataURLFromImage(img){
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        canvas.width = img.naturalWidth;
+        canvas.height = img.naturalHeight;
+        ctx.drawImage(img, 0, 0);
+        return canvas.toDataURL();
+    }
     static toDataURLFromFile(file){
         return new Promise((resolve) => {
             const reader = new FileReader();
@@ -9,13 +17,13 @@ export class B64 {
             reader.readAsDataURL(file);
         });
     }
-
-    static toDataURLFromImage(img){
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d');
-        canvas.width = img.naturalWidth;
-        canvas.height = img.naturalHeight;
-        ctx.drawImage(img, 0, 0);
-        return canvas.toDataURL();
+    static toDataURLFromPath(path){
+        return new Promise((resolve) => {
+            const img = new Image();
+            img.addEventListener('load', () => {
+                resolve(B64.toDataURLFromImage(img));
+            }, false);
+            img.src = path;
+        });
     }
 }
